9000dd9cd060c78f7d3fe55c79b7f49fbe856434
divy vinayak <divyvinayak@gmail.com>
2023-11-30 02:33:53 +0530

    index page with signIn and signUp button made. onclick logic is to be applied next

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is main app page.")   
+    return render(request, 'app/index.html')
